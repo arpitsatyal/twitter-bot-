a54664ea577a50ab8c52b3d78d49d0214a825aee
Arpit Satyal <arpited7@gmail.com>
2020-02-22 17:25:52 +0545

    sending multiple random tweets

--- randomBot.js
+++ randomBot.js
@@ -5,40 +5,47 @@ let getArtists = require('./spotify')
 
 let bestOrWorst = ['Best', 'worst']
 let isBetter = ['better', 'worse']
+let isBommer = ['Boomer', 'Zoomer']
 
-async function read2 () {
+async function read2() {
     setInterval(tweetIt, 5000)
 }
 read2()
 
 function createRandom(value) {
-    let r 
-    typeof(value) === 'number' ? r =  Math.floor(Math.random() * value) :  r =  Math.floor(Math.random() * value.length)
+    let r
+    typeof (value) === 'number' ? r = Math.floor(Math.random() * value) : r = Math.floor(Math.random() * value.length)
     return r
 }
 
 async function tweetIt() {
     try {
-    let allBands = await getArtists()
-    let random = createRandom(allBands)
-    let randomTwo =  createRandom(allBands)
-    let randomThree = createRandom(bestOrWorst)
-    let randomFour = createRandom(bestOrWorst)
+        let allBands = await getArtists()
+        let random = createRandom(allBands)
+        let randomTwo = createRandom(allBands)
+        let randomThree = createRandom(bestOrWorst)
+        let randomFour = createRandom(bestOrWorst)
 
-    let tweet = {
-       status: `the ${bestOrWorst[randomThree]} ${allBands[random]} album is ${isBetter[randomThree]} than the ${bestOrWorst[randomFour]} ${allBands[randomTwo]} album`
-    }
+        let tweetsArr = [`the ${bestOrWorst[randomThree]} ${allBands[random]} album is ${isBetter[randomThree]} than the ${bestOrWorst[randomFour]} ${allBands[randomTwo]} album`,
+        `${allBands[random]} is literally just ${allBands[randomTwo]}`,
+        `${allBands[random]} is just ${allBands[randomTwo]} for ${isBommer[randomThree]}`,
+        `${allBands[randomTwo]} is for people who dont get invited to parties`]
 
-    T.post('statuses/update', tweet, tweeted)
-
-    function tweeted(err, data, response) {
-        if (err) {
-            console.log('something went wrong..', err)
-        } else {
-            console.log('tweet sent!')
+        let toSendRandom = createRandom(tweetsArr)
+        let tweet = {
+            status: tweetsArr[toSendRandom]
         }
-    }
-} catch (e) { console.log(e) }
+
+        T.post('statuses/update', tweet, tweeted)
+
+        function tweeted(err, data, response) {
+            if (err) {
+                console.log('something went wrong..', err)
+            } else {
+                console.log('tweet sent!')
+            }
+        }
+    } catch (e) { console.log(e) }
 }
 
 
